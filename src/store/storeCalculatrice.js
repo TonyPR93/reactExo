952b268c45f670f3e499015e130807dcd3dd5a1a
Panont Anthony <anthonypanont@gmail.com>
2023-05-16 23:53:58 +0200

    Ajout store clock

--- src/store/storeCalculatrice.js
+++ src/store/storeCalculatrice.js
@@ -1,12 +1,16 @@
 import { createStore, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import reducerCalculatrice from '../reducers/reducerCalculatrice';
-import reducerExplication from '../reducers/reducerExplication'
+import reducerExplication from '../reducers/reducerExplication';
 
-import reducerRandomQuote from '../reducers/reducerRandomQuote'
+import reducerRandomQuote from '../reducers/reducerRandomQuote';
+
+import reducerClock from '../reducers/reducerClock';
 const rootReducers = combineReducers({
     calculatrice: reducerCalculatrice,
     explication: reducerExplication,
-    randomQuote: reducerRandomQuote
+    randomQuote: reducerRandomQuote,
+    clock: reducerClock
 })
 
 const store = createStore(rootReducers);
